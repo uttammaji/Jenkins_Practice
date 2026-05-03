74d67e8d020ccb4a74e5eb6a3c3e51de88f5ed37
Uttam Maji <uttammaji842@gmail.com>
2026-05-03 14:07:24 +0530

    Update7 Jenkins.py

--- Jenkins.py
+++ Jenkins.py
@@ -1,5 +1,5 @@
 num=int(input("Enter a number that is need number for change: "
 if num%2==0
-    print(num,"is an even number all time.")
+    printnum,"is an even number all time.")
 else
     printnum,"is an odd number all time."
